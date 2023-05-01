e8a763d57833efe3b48baffa0585367691c8f17e
pc <EduardoLJ>
2023-05-01 02:10:58 -0600

    Initial commit

---
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.superheroes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.superheroes.model.RepositorioHeroes
+import com.example.superheroes.ui.theme.SuperheroesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SuperheroesTheme {
+                AppSuperheroes()
+            }
+        }
+    }
+}
+
+@Composable
+fun AppSuperheroes() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            BarraSuperior()
+        }
+    ) {
+        /* Important: It is not a good practice to access data source directly from the UI.
+        In later units you will learn how to use ViewModel in such scenarios that takes the
+        data source as a dependency and exposes heroes.
+         */
+        val heroes = RepositorioHeroes.heroes
+        ListaHeroes(heroes = heroes, Modifier.padding(it))
+
+    }
+}
+
+@Composable
+fun BarraSuperior(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(56.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.h1,
+        )
+    }
+}
---
+++ app/src/main/java/com/example/superheroes/ui/theme/Color.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022 The Android Open Source Project
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.superheroes.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+//Light Theme
+val md_theme_light_background = Color(0xEEECEBE3)
+val md_theme_light_surface = Color(0xEEDFD9BD)
+val md_theme_light_secondary = Color(0xEE485037)
+val md_theme_light_onSurface = Color(0xEE0A0B07)
+val md_theme_light_primary = Color(0xEE3557FF)
+val md_theme_light_onPrimary = Color(0xEE1125FF)
+
+
+// Dark Theme
+val md_theme_dark_background = Color(0xEE0A0B07)
+val md_theme_dark_surface = Color(0xEE262E18)
+val md_theme_dark_secondary = Color(0xEECCD5B5)
+val md_theme_dark_onSurface = Color(0xEED3D2CA)
+val md_theme_dark_primary = Color(0xEEB0B99A)
+val md_theme_dark_onPrimary = Color(0xEECCD5B5)
